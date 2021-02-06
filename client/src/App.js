7b1a445086e0f269d8c48b092f1fc0c917d9884b
Ibrameias <ibramdarwish@gmail.com>
2021-02-06 17:14:27 -0500

    install dependencies to jason file

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import React from "react"
+import Search from "./pages/Search"
+import Saved from "./pages/Saved"
+import Navigation from "./components/Navigation/Navigation"
+import { BrowserRouter as Router, Route} from "react-router-dom"
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <Navigation />
+      
+      <Route exact path="/" component={Search} />
+      <Route path="/Saved" component={Saved} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
